066867185c4404e30edf7e74501daf6811c2ca3d
sudhakarsharma07 <117532270+sudhakarsharma07@users.noreply.github.com>
2023-02-16 23:09:44 +0530

    Create Day3_series9.c

---
+++ Day3_series9.c
@@ -0,0 +1,16 @@
+# Problem-solving
+#include<stdio.h>
+void main()
+{
+    int i,j,N;
+    printf("Enter the value for N:");
+    scanf("%d",&N);
+    for(i=1;i<=N;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf(" ");
+    }
+}
